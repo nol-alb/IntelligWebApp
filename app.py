91fb5088ebd38fce76b0c63f16c25848ace94a60
Noel Alben <65276123+nol-alb@users.noreply.github.com>
2023-04-26 03:25:03 -0400

    app back to release

--- app.py
+++ app.py
@@ -389,5 +389,5 @@ def register():
     return render_template('registration.html', form=form)
 
 if __name__ == "__main__":
-    #app.run(host='130.207.85.75', port = 5000)
-    app.run()
+    app.run(host='130.207.85.75', port = 5000)
+    #app.run()
